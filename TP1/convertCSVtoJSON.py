dfd4ed3e9a1d11b157507dbf62ddfba677048a00
ArmandoBSilva99 <armandob.silva99@gmail.com>
2022-03-18 00:15:50 +0000

    header updates

--- TP1/convertCSVtoJSON.py
+++ TP1/convertCSVtoJSON.py
@@ -4,9 +4,21 @@ from turtle import position
 
 #ER = r'"a[0-9]+","[A-Za-z éáçãíÉÁÇÃÍóô\-âï]+","[A-Z]+"[,\d]+\n'
 
-def head_reader(header):
-    return re.split(r',',header)
-
+def head_reader(header, categories):
+    #aggregation e.g. Notas{4,5}
+    aggregatedcategories = re.findall(r'(\w+{[\d,]+}(::\w+)*|\w+{[\d,]+})', header)
+    print("aggregated categories: ")
+    print(aggregatedcategories)
+    aggregation = re.sub(r'\w+{[\d,]+}(::\w+)*', r'',header)
+    print("normal categories: ")
+    print(aggregation)
+    """
+    if(aggregation):
+        for cat in aggregation:
+            categories.append( (re.findall(r'^\w+', cat)[0], re.findall(r'\d+', cat)) )
+        print(categories)
+    res = re.split(r',',header)
+    """
 def converter(lines, headers):
     result = "[\n"
     i = 0
@@ -30,12 +42,13 @@ def converter(lines, headers):
     result += "]\n"
     return result  
 
-f = open("alunos.csv")
+f = open("teste.csv")
 lines = f.read().splitlines()
 f.close()
-header = head_reader(lines[0]) #HEADER
-result = converter(lines[1:], header)
-f = open("result.json","w+")
-f.write(result)
-f.close()
+categories = []
+header = head_reader(lines[0], categories) #HEADER
+#result = converter(lines[1:], header)
+#f = open("result.json","w+")
+#f.write(result)
+#f.close()
 
