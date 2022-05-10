0dab6e799107d5439bccaff05c805d996b907434
joanaaVO <joanaveigaoliveira@gmail.com>
2022-05-10 01:20:26 +0100

    alguns progressos na leitura da parte inicial

--- TP2/conversor_lex.py
+++ TP2/conversor_lex.py
@@ -2,145 +2,139 @@ import re
 import ply.lex as lex
 from urllib3 import Retry
 
-tokens = ['ID', 'STRING', 'PERC', "ER", "RETURN", "PAL", "ERROR", "COMMENT", "LIST", "END", "EMPTYLIST", "EXP", "YACC", "TEXT", "PYTHON", "com", "FUNCTION"]
+tokens = ['LEX', 'YACC', 'FUNCTIONS', 'ERS', 'ID', 'STRING', 'PERC', "ER", "RETURN", "PAL", "ERROR", "COMMENT", "LIST", "END", "EMPTYLIST", "EXP", "TEXT", "PYTHON", "com"]
 literals = ['=', '(', ')', '[', ']', ',']
 states = [("var", "exclusive"), ("func", "exclusive"), ("er", "exclusive"), ("python", "exclusive")]
 
-def t_lex(t):
+def t_LEX(t):
     r'%%\sLEX'
-    ##print("LEX: " + t.value)
-    #t.lexer.begin("lex")
+    ###print("LEX: " + t.value)
+    return t
 
-def t_yacc(t):
+def t_YACC(t):
     r'%%\sYACC'
-    print("YACC: " + t.value)
-    #t.lexer.begin("yacc")
+    #print("YACC: " + t.value)
+    return t
 
-def t_FUNCTION(t):
+def t_FUNCTIONS(t):
     r'%%\sFUNCTIONS'
-    print("FUNCTIONS: " + t.value)
+    #print("FUNCTIONS: " + t.value)
     t.lexer.begin("func")
+    return t
 
-def t_ers(t):
+def t_ERS(t):
     r'%%\sERS'
-    print("ERS: " + t.value)
+    #print("ERS: " + t.value)
     t.lexer.begin("er")
+    return t
 
 def t_com(t):
     r'\#.*'
-    print("COMMENT: " + t.value)
+    #print("COMMENT: " + t.value)
     return t
 
 def t_PERC(t):
     r'%'
     t.lexer.begin("var")
-    print("PERC: " + t.value)
+    #print("PERC: " + t.value)
     return t
 
 def t_var_YACC(t):
     r'yacc\(\)'
-    print("YACC: " + t.value)
+    #print("YACC: " + t.value)
     t.lexer.begin("python")
     return t      
 
 def t_var_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
-    print("ID: " + t.value)
+    #print("ID: " + t.value)
     return t
 
 def t_var_STRING(t):
     r'\".*\"'
-    print("STRING: " + t.value)
+    #print("STRING: " + t.value)
     t.lexer.begin("INITIAL")
     return t
 
 def t_var_LIST(t):
     r'\[.*\]'
-    print("LIST: " + t.value)
+    #print("LIST: " + t.value)
     t.lexer.begin("INITIAL")
     return t
 
 def t_var_COMMENT(t):
     r'\#.*'
-    print("VAR_COMMENT: " + t.value)
+    #print("VAR_COMMENT: " + t.value)
     return t
 
 def t_var_EMPTYLIST(t):
     r'\{\}'
-    print("EMPTYLIST: " + t.value)
+    #print("EMPTYLIST: " + t.value)
     t.lexer.begin("INITIAL")
     return t
   
-
-#def t_PERC(t):
-#    r'%'
-    #t.lexer.push_state("var")
-#    #print("PERC: " + t.value)
-#    return t
-
 def t_func_RETURN(t):
     r'return'
-    print("RETURN: " + t.value)
+    #print("RETURN: " + t.value)
     return t
 
 def t_func_ERROR(t):
     r'error'
-    print("ERROR: " + t.value)
+    #print("ERROR: " + t.value)
     return t
 
 def t_func_END(t):
     r'%%\sEND'
-    print("END: " + t.value)
+    #print("END: " + t.value)
     t.lexer.begin("INITIAL")
     return t
 
 def t_func_STRING(t):
     r'\".*\"'
-    print("STRING: " + t.value)
+    #print("STRING: " + t.value)
     return t
 
 def t_func_PAL(t):
     r'[a-zA-Z0-9\.\(\)]+'
-    print("PAL: " + t.value)
+    #print("PAL: " + t.value)
     return t
 
 def t_func_ER(t): 
     r'[^\s]+'
-    print("FUNC_ER: " + t.value)
-    #t.lexer.begin("func")
+    #print("FUNC_ER: " + t.value)
     return t
 
 def t_er_STRING(t):
     r'\".*\"'
-    print("STRING: " + t.value)
+    #print("STRING: " + t.value)
     return t
 
 def t_er_PYTHON(t):
     r'%%\sPYTHON'
-    print("PYTHON BEGIN: " + t.value)
+    #print("PYTHON BEGIN: " + t.value)
     t.lexer.begin("python")
     return t 
 
 def t_er_EXP(t):
     r'[^\}\"\:]+'
-    print("EXP: " + t.value)
+    #print("EXP: " + t.value)
     return t
 
 def t_python_END(t):
     r'%%'
-    print("END: " + t.value)
+    #print("END: " + t.value)
     t.lexer.begin("INITIAL")
     return t
 
 def t_python_PERC(t):
     r'%'
-    print("PYTHON PERC: " + t.value)
+    #print("PYTHON PERC: " + t.value)
     t.lexer.begin("var")
     return t
 
 def t_python_TEXT(t):
     r'.+'
-    print("TEXT: " + t.value)
+    #print("TEXT: " + t.value)
     return t
 
 t_ignore = " \t\n\r"
@@ -148,10 +142,6 @@ t_var_ignore = " \t\n\r"
 t_func_ignore = " \t\n\r,'()"
 t_er_ignore = " \t\n\r}{:"
 t_python_ignore = " \t\n\r"
-
-#def t_lex_error(t):
-#    #print("Illegal character: ", t.value[0])
-#    t.lexer.skip(1)
 
 def t_var_error(t):
     print("Illegal character: ", t.value[0])
